a80488bf97a44270e3130ad08018380b3b7853bd
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-31 18:33:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+  attr_writer :bank_account
+  
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bank_account
+    25
+  end
+  
+  def happiness
+    8
+  end
+  
+end
